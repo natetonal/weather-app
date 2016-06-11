d8889140385e1a9e9222c3fec2a851035ea883e3
Nate Kimball <nsktrombone84@hotmail.com>
2016-06-10 18:43:33 -0700

    fixed About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,12 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page.</p>
+        <h1 className="text-center">About</h1>
+        <p>
+        This is a really cool weather app. This is actually my 3rd weather app now. I feel like there's a conspiracy about
+        teachers forcing students into creating weather apps from OpenWeatherMap. I think I get the idea by now, but it's
+        always still good to go through it again just to make sure there's nothing I've missed.
+        </p>
     </div>
   )
 };
